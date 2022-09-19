4f11a71d2f8f9170af0b746e0b57d79f713f844d
CAPPELLE A <cappelleaymeric@gmail.com>
2022-09-19 09:02:00 +0200

    Update

--- main.js
+++ main.js
@@ -4,31 +4,47 @@ function showRangeValue(value){
 }
 
 function genPass(){
-    var numbers = "0123456789";
-    var string = "abcdefghijklmnpqrstuvwxyz";
-    var symbols = "#!+=%";
-    var upper = "AZERTYUIPQSDFGHJKLMWXCVBN";
-    const passLength = document.getElementById('rangeLength').value;
+    let numbers = "0123456789";
+    let defaultstring = "abcdefghijklmnpqrstuvwxyz";
+    let symbols = "@!";
+    let upper = "AZERTYUIPQSDFGHJKLMWXCVBN";
+    let passLength = document.getElementById('rangeLength').value;
     const ifuppercase = document.querySelector('#checkUppercase');
     const ifnumbers = document.querySelector('#checkNumbers');
     const ifsymbols = document.querySelector('#checkSymbol');
-
-    if(ifnumbers.checked == true){
-        string = string + numbers;
-        console.log(string);
-    }else{
-        if(ifsymbols.checked == true){
-            string = string + symbols;
-        }else{
-            if(ifuppercase.checked == true){
-                string = string + upper;
+    let password = "";
+    let string = defaultstring;
+    let array = [ifuppercase, ifnumbers, ifsymbols];
+    let i = 0;
+    while(i < array.length){
+        if(array[i].checked == true){
+            value = array[i].value;
+            switch(value){
+                case 'upper':
+                    string = upper + string;
+                    break;
+                case 'numbers':
+                    string = numbers + string;
+                    break;
+                case 'symbols':
+                    string = symbols + string;
+                    break;
             }
         }
+        i++;
     }
-    document.getElementById('generatedPassword').innerText = `${string}`;
-    return console.log('OK');
+    for (let i2 = 0; i2 < passLength; i2++) {
+        password += string[Math.floor(Math.random() * string.length)]
+      }
+    document.getElementById('generatedPassword').value = `${password}`;
+    return console.log(password);
+}
 
-   
-
-    
+function copyPass(){
+    const copyText = document.getElementById("generatedPassword");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copy password: " + copyText.value);
+    console.log('Copy password ! ');
 }
